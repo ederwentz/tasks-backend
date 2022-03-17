2bdb0c6dde033f3fb64ca1a877fb93fd06ebabc0
Eder Wentz <eder.wentz@gmail.com>
2022-03-17 14:22:47 -0300

    src/test/ Junit

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -0,0 +1,30 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DateUtilsTest {
+	
+	@Test
+	public void deveRetornarVeTrueParaDatasFuturas() {
+		LocalDate date = LocalDate.of(2030, 01, 01);
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+		//System.out.println(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void deveRetornarVeFalseParaDatasPassadas() {
+		LocalDate date = LocalDate.of(2010, 01, 01);
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+		//System.out.println(DateUtils.isEqualOrFutureDate(date));
+	}
+	
+	@Test
+	public void deveRetornarVeTrueParaDatasAtual() {
+		LocalDate date = LocalDate.now();
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+		//System.out.println(DateUtils.isEqualOrFutureDate(date));
+	}
+}
